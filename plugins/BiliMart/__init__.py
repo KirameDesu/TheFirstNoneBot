a405cca9e7daeb1f517f2d9ccd894b48b8adbf19
RFN_KIRAME <13787701007@sina.cn>
2024-10-17 14:00:04 +0800

    1.进一步完善BiliMart插件

--- plugins/BiliMart/__init__.py
+++ plugins/BiliMart/__init__.py
@@ -1,37 +1,100 @@
 import re
+import argparse
+import shlex
+from logging import lastResort
 
 from nonebot.adapters import Message
 from nonebot.adapters import Event
 from nonebot.internal.params import ArgPlainText, Arg
 from nonebot.log import default_format
 from nonebot.params import CommandArg, Received, LastReceived
+from nonebot.matcher import Matcher
 
 from nonebot import on_command, logger
+
+from plugins.BiliMart.bilimart import get_match_items_str, append_keyword, set_min_price, set_max_price, set_bot_matcher
 
 mc_biliMart = on_command("bilimart")
 
 @mc_biliMart.handle()
-async def _(args: Message = CommandArg()):
+async def _(marcher: Matcher, args: Message = CommandArg()):
+    set_bot_matcher(marcher)
     if para := args.extract_plain_text():
-        await mc_biliMart.finish(f"关键字{para}");
+        if para == "go":
+        # await mc_biliMart.finish(f"关键字{para}");
+            await mc_biliMart.finish(get_match_items_str())
+        else:
+            try:
+                args = parse_command_line(para)
+                matched_str = await do_bilimart(args.minprice, args.maxprice, args.keyword)
+                if matched_str != "":
+                    await mc_biliMart.finish(matched_str)
+                else:
+                    await mc_biliMart.finish("什么都没有找到哦")
+            except ValueError:
+                await mc_biliMart.finish("这是不被识别的指令格式哦")
+
 
 
 @mc_biliMart.got("keyword", prompt="给我一个关键字")
 async def _(keyword: str = ArgPlainText()):
-    logger.add("keyword=" + keyword, level="DEBUG", format=default_format, rotation="1 week")
+    logger.info("keyword=" + keyword)
     if not re.fullmatch(r'[\u4e00-\u9fff]+', keyword):
         await mc_biliMart.reject("只能输入中文哦")
 
 
-@mc_biliMart.got("max_money", prompt="最大金额是多少呢")
-async def _(max_money: str = ArgPlainText()):
-    logger.add("max_money=" + max_money, level="DEBUG", format=default_format, rotation="1 week")
-    if not re.fullmatch(r'[0-9]+', max_money):
+@mc_biliMart.got("min_money", prompt="最小金额是多少呢")
+async def _(min_money: str = ArgPlainText()):
+    logger.info("max_money=" + min_money)
+    if not re.fullmatch(r'[0-9]+', min_money):
         await mc_biliMart.reject("只能输入数字哦")
 
 
-@mc_biliMart.receive()
-async def receive_func(event: Event = LastReceived()):
-    if event.get_type() == "message":
-        await mc_biliMart.reject("戳我一下")
-    await mc_biliMart.finish("Roger~")
+@mc_biliMart.got("max_money", prompt="最大金额是多少呢")
+async def _(matcher: Matcher, max_money: str = ArgPlainText()):
+    logger.info("max_money=" + max_money)
+    if not re.fullmatch(r'[0-9]+', max_money):
+        await mc_biliMart.reject("只能输入数字哦")
+    else:
+        minp = matcher.get_arg("min_money", default=0)
+        maxp = matcher.get_arg("max_money", default=1500)
+        kw = matcher.get_arg("keyword", default="")
+        await mc_biliMart.finish(await do_bilimart(minp, maxp, kw))
+
+
+
+async def do_bilimart(min_price: float, max_price: float, keyword: str)-> str:
+    set_min_price(min_price)
+    set_max_price(max_price)
+    append_keyword(keyword)
+    logger.info(f"开始查询最小金额{min_price},最大金额{max_price},关键词\"{keyword}\"的商品...")
+    try:
+        item_str = await get_match_items_str()
+        return item_str
+    except ValueError as e:
+        logger.error(f"爬取函数执行出错--{e}")
+        return ""
+
+# @mc_biliMart.receive()
+# async def receive_func(event: Event = LastReceived()):
+#     if event.get_type() == "message":
+#         await mc_biliMart.reject("戳我一下")
+#     await mc_biliMart.finish("Roger~")
+
+pattern = r'^--minprice\s+\d+(\.\d+)?\s+--maxprice\s+\d+(\.\d+)?\s+--keyword\s+.+$'
+def parse_command_line(input_str):
+    # 检查输入字符串是否包含所需的参数
+    if not re.match(pattern, input_str.strip()):
+        raise ValueError("Input string must contain --minprice, --maxprice, and --keyword.")
+
+    # 将输入字符串转换为适合 argparse 的格式
+    args_list = shlex.split(input_str)
+
+    parser = argparse.ArgumentParser(description='Parse command line arguments for min price, max price, and keyword.')
+    parser.add_argument('--minprice', type=float, help='Minimum price', required=True)
+    parser.add_argument('--maxprice', type=float, help='Maximum price', required=True)
+    parser.add_argument('--keyword', type=str, help='Keyword', required=True)
+
+    # 解析参数
+    args = parser.parse_args(args_list)
+    return args
--- plugins/BiliMart/bilimart.py
+++ plugins/BiliMart/bilimart.py
@@ -1,6 +1,14 @@
 # ------------------------- Configuration Modification -------------------------
 # 请填写 Bilibili Cookie 至下方引号内
-BILIBILI_COOKIE = "buvid4=5B6A3A11-7A2B-D09D-3DD2-1D8C3DD3F36630667-022120611-hVn2U35PvWhS3UuTmAFR1g%3D%3D; header_theme_version=CLOSE; CURRENT_FNVAL=4048; is-2022-channel=1; buvid_fp_plain=undefined; enable_web_push=DISABLE; hit-dyn-v2=1; PVID=1; CURRENT_QUALITY=64; FEED_LIVE_VERSION=V_WATCHLATER_PIP_WINDOW3; buvid3=C3D2E1D5-0BF2-9DF7-F330-96214E9CC11F60521infoc; b_nut=1725433360; _uuid=E28E1CD4-D6E4-9AB3-9F99-8889B1D65EF760750infoc; fingerprint=7c60a2c8264c8cb61673c5b6fe173a59; buvid_fp=7c60a2c8264c8cb61673c5b6fe173a59; b_lsid=33B6A1018_1928E76C3B9; bsource=search_bing; bmg_af_switch=1; bmg_src_def_domain=i1.hdslb.com; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjkyMjYyMDMsImlhdCI6MTcyODk2Njk0MywicGx0IjotMX0.FMlb17C6NkexEfM5FbkWdk0ApgT7iqdxw1EZIWIzERo; bili_ticket_expires=1729226143; SESSDATA=3ef6035b%2C1744519138%2C36012%2Aa2CjAJCHreOILUWylqvjh6Mc8xo__ZQve5Pd0NUgYZ3rj0hLX6kGFUxA6Ds_fz8aXby7YSVnU5SWJxT0h1cFc1QW5uUkx2NHVBdzI3WGFreFdkMzI3R3ZHQ2RfUGxyTEJBdVBtc2lleGlCc1ptUV95UjAtekdoMDRQWjNxdU1kenFIVGozXzA0U0hBIIEC; bili_jct=40ffb5a3a8ba590d957e91d03a73f6c2; DedeUserID=3546779823900918; DedeUserID__ckMd5=3a5547f69f307f08; sid=8fp3bwaf; home_feed_column=4; browser_resolution=1100-2417"
+import re
+import random
+from asyncio import sleep
+from typing import Optional
+
+from nonebot import logger
+from nonebot.matcher import Matcher
+
+BILIBILI_COOKIE = "buvid4=5B6A3A11-7A2B-D09D-3DD2-1D8C3DD3F36630667-022120611-hVn2U35PvWhS3UuTmAFR1g%3D%3D; header_theme_version=CLOSE; CURRENT_FNVAL=4048; is-2022-channel=1; buvid_fp_plain=undefined; enable_web_push=DISABLE; hit-dyn-v2=1; PVID=1; CURRENT_QUALITY=64; FEED_LIVE_VERSION=V_WATCHLATER_PIP_WINDOW3; buvid3=C3D2E1D5-0BF2-9DF7-F330-96214E9CC11F60521infoc; b_nut=1725433360; _uuid=E28E1CD4-D6E4-9AB3-9F99-8889B1D65EF760750infoc; fingerprint=7c60a2c8264c8cb61673c5b6fe173a59; buvid_fp=7c60a2c8264c8cb61673c5b6fe173a59; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjkyMjYyMDMsImlhdCI6MTcyODk2Njk0MywicGx0IjotMX0.FMlb17C6NkexEfM5FbkWdk0ApgT7iqdxw1EZIWIzERo; bili_ticket_expires=1729226143; SESSDATA=3ef6035b%2C1744519138%2C36012%2Aa2CjAJCHreOILUWylqvjh6Mc8xo__ZQve5Pd0NUgYZ3rj0hLX6kGFUxA6Ds_fz8aXby7YSVnU5SWJxT0h1cFc1QW5uUkx2NHVBdzI3WGFreFdkMzI3R3ZHQ2RfUGxyTEJBdVBtc2lleGlCc1ptUV95UjAtekdoMDRQWjNxdU1kenFIVGozXzA0U0hBIIEC; bili_jct=40ffb5a3a8ba590d957e91d03a73f6c2; DedeUserID=3546779823900918; DedeUserID__ckMd5=3a5547f69f307f08; sid=8fp3bwaf; bsource=search_bing; bmg_af_switch=1; bmg_src_def_domain=i1.hdslb.com; rpdid=0zbfVGpAUa|hSgk3Z8E|1IA|3w1T0ZeJ; deviceFingerprint=4e6d0e71273b181658ce76b5d483db72; kfcFrom=mall_search_mall; from=mall_search_mall; b_lsid=E4E6CD82_19294DCAAB7; bp_t_offset_3546779823900918=988855849843687424; home_feed_column=4; browser_resolution=1060-2329"
 # ------------------------------------------------------------------------------
 
 import colorama
@@ -8,24 +16,77 @@ from colorama import Fore, Style
 import time
 import json
 import requests
-from tkinter import filedialog
 from datetime import datetime
 import os
 import pandas as pd
-import tkinter as tk
 
-def crawler():
-    global category_entry, output_dir, min_price, max_price, showPrice, start_time, item_count, pause_count, retry_count
+MAX_SEARCH_TIME = 2000
+
+g_min_price = 0
+g_max_price = 5000
+min_discount = 0
+max_discount = 90
+category_entry_value = "手办"
+
+
+
+class Item:
+    def __init__(self, name: str = None, price: float = None, id: str = None):
+        self.name = name
+        self.price = price
+        self.id = id
+
+    def __str__(self):
+        return f"Item(Name: {self.name}, Price: {self.price}, ID: {self.id})"
+
+
+class BiliMart:
+    def __init__(self):
+        self.min_price = None
+        self.max_price = None
+        self.min_discount = None
+        self.max_discount = None
+        self.category = None
+
+        self.keywords: list[str] = []
+
+        self.max_match_time = 4
+        self.curr_match_time = 0
+        self.match_items: list[Item] = []
+
+        self.curr_search_time = 0
+
+    def add_match_item(self, item: Item):
+        self.match_items.append(item)
+
+    def print(self) -> str:
+        item_strings = []
+        for item in self.match_items:
+            item_strings.append(str(item))
+        self.match_items.clear()
+
+        return "\n".join(item_strings)
+
+    def reset(self):
+        self.curr_search_time = 0
+        self.curr_match_time = 0
+        self.keywords.clear()
+
+bilimart = BiliMart()
+matcher: Optional[Matcher] = None
+
+async def crawler():
+    global category_eatry, output_dir, g_min_price, g_max_price, showPrice, start_time, item_count, pause_count, retry_count
 
     mall_url = "https://mall.bilibili.com/mall-magic-c/internet/c2c/v2/list"
 
-    min_price = min_price_entry.get() if min_price_entry.get() else "0"
-    max_price = max_price_entry.get() if max_price_entry.get() else "5000"
-    min_discount = min_discount_entry.get() if min_discount_entry.get() else "0"
-    max_discount = max_discount_entry.get() if max_discount_entry.get() else "100"
+    # g_min_price = min_price_entry.get() if min_price_entry.get() else "0"
+    # g_max_price = max_price_entry.get() if max_price_entry.get() else "5000"
+    # min_discount = min_discount_entry.get() if min_discount_entry.get() else "0"
+    # max_discount = max_discount_entry.get() if max_discount_entry.get() else "100"
 
     # 将最低价格和最高价格乘以 100 并格式化为字符串
-    setprice = "{}-{}".format(int(float(min_price) * 100), int(float(max_price) * 100 + 1))
+    setprice = "{}-{}".format(int(float(g_min_price) * 100), int(float(g_max_price) * 100 + 1))
     # 将最低折扣和最高折扣格式化为字符串
     discount = "{}-{}".format(int(min_discount), int(max_discount) + 1)
 
@@ -38,16 +99,20 @@ def crawler():
         "福袋": "fudai_cate_id"
     }
 
-    category_entry_value = category_entry.get()
+    # category_entry_value = category_entry.get()
     category_entry_real = category_entry_value.replace("c", "C")
     ID = id_mapping.get(category_entry_real, "")
     item_list = []
     item_count = pause_count = retry_count = 0
-    showPrice = min_price
+    showPrice = g_min_price
     nextId = None
     start_time = time.perf_counter()
 
     while True:
+        # 结束爬取条件
+        if is_to_end_crawler():
+            break
+
         payload = json.dumps({
             "sortType": "PRICE_ASC",
             "priceFilters": [
@@ -99,12 +164,19 @@ def crawler():
                     "卖家名称": uname,
                     "卖家UID": uid
                 })
+                # 检查商品名称中是否包含关键字
+                if is_match_keywords(c2cItemsName):
+                    bilimart.add_match_item(Item(c2cItemsName, showPrice, c2cItemsId))
                 item_count += 1
                 retry_count = 0
 
+            if item_count % 300 == 0:
+                await matcher.send(f"刷新到了{item_count}个商品，正在查找符合关键字的商品中...")
+
+            bilimart.curr_search_time += 1
+            time.sleep(random.randint(2,10))
             if nextId is None:
                 break
-
         except (TypeError, KeyError) as e:
             try:
                 if response["code"]:
@@ -112,6 +184,7 @@ def crawler():
                         pause_count += 1
                         print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                         print(f'{Fore.YELLOW}[Error {response["code"]}] 检测到爬取器触发操作频繁提示, 暂停 30 秒后重试...(¥{showPrice}){Style.RESET_ALL}')
+                        # await matcher.send("被检测到频繁操作，只能30s后再拉取了...")
                         information_values_output(0)
                         print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                         time_sleep_10ns(3)
@@ -120,6 +193,7 @@ def crawler():
                             print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                             print(f'{Fore.RED}[Error {response["code"]}] 检测到账号已被风控! 请更换 IP 后重试...{Style.RESET_ALL}')
                             print(f'{Fore.YELLOW}[WARN] 从现在起, 爬取器每暂停 60 秒都将重试, 连续失败 5 次后认定爬取失败, 程序自动退出{Style.RESET_ALL}') if retry_count == 0 else ""
+                            # await matcher.send("切换为60s拉取一次...")
                             information_values_output(1)
                             print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                             if retry_count < 5:
@@ -142,6 +216,7 @@ def crawler():
                     elif response["code"] == 83001002:
                         print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                         print(f'{Fore.RED}[Error {response["code"]}] 检测到 Bilibili Cookie 配置不正确! 请检查后重试...{Style.RESET_ALL}')
+                        await matcher.send("cookie配置有问题了哦")
                         print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                         exit()
                     else:
@@ -159,6 +234,7 @@ def crawler():
                 pause_count += 1
                 print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                 print(f'{Fore.YELLOW}[Error Network] 检测到网络环境不稳定, 暂停 5 秒后重试...{Style.RESET_ALL}')
+                # await matcher.send("网络环境不稳定, 5秒后再试试...")
                 information_values_output(0)
                 print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                 time_sleep_5s()
@@ -170,6 +246,7 @@ def crawler():
             try:
                 print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
                 print(f'{Fore.RED}[Error 412] 检测到账号已被风控! 请更换 IP 后重试...{Style.RESET_ALL}')
+                await matcher.send("哦豁!检测到账号已被风控")
                 print(f'{Fore.YELLOW}[WARN] 从现在起, 爬取器每暂停 60 秒都将重试, 连续失败 5 次后认定爬取失败, 程序自动退出{Style.RESET_ALL}') if retry_count == 0 else ""
                 information_values_output(1)
                 print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
@@ -192,15 +269,18 @@ def crawler():
             except_error_null(e)
             break
 
+    '''
     information_values_output(3)
-    output_dir = filedialog.askdirectory()
 
-    if not os.path.isdir(output_dir):
-        print(f'{Fore.CYAN}[INFO] 商品信息的保存被取消!{Style.RESET_ALL}')
-        return
+    # if not os.path.isdir(output_dir):
+    #     print(f'{Fore.CYAN}[INFO] 商品信息的保存被取消!{Style.RESET_ALL}')
+    #     return
 
     current_time = datetime.now().strftime("%Y-%m-%d_%H时%M分%S秒")
-    file_name = f'{current_time}_{min_price}-{max_price}元_{int(min_discount) // 10}-{int(max_discount) // 10}折{category_entry_real}.xlsx'
+    file_name = f'{current_time}_{g_min_price}-{g_max_price}元_{int(min_discount) // 10}-{int(max_discount) // 10}折{category_entry_real}.xlsx'
+    current_dir = os.getcwd()
+    output_dir = os.path.join(current_dir, "xml")
+    os.makedirs(output_dir, exist_ok=True)
     output_file = os.path.normpath(os.path.join(output_dir, file_name))
 
     df = pd.DataFrame(item_list, columns = ["商品编号", "商品名称", "当前价格", "市场价格", "卖家名称", "卖家UID"])
@@ -245,7 +325,9 @@ def crawler():
 
     writer.close()
     print(f'{Fore.CYAN}[INFO] 商品信息已保存至 {output_file}{Style.RESET_ALL}')
+    '''
     pass
+
 
 def extract_minutes_seconds(interval_time):
     min = int(interval_time // 60)
@@ -272,30 +354,30 @@ def calculate_weighted_progress(showPrice):
     return 1.0  # 如果价格超过最高区间，则进度为100%
 
 def information_values_output(tag):
-    global max_price, showPrice
+    global g_max_price, showPrice
 
-    max_price = str(float(max_price) + 1) if max_price == min_price else max_price
-    showPrice = str(float(showPrice) + 1) if showPrice == min_price else showPrice
+    g_max_price = str(float(g_max_price) + 1) if g_max_price == g_min_price else g_max_price
+    showPrice = str(float(showPrice) + 1) if showPrice == g_min_price else showPrice
 
-    if min_price == "0" and max_price == "5000":
+    if g_min_price == "0" and g_max_price == "5000":
         progress = calculate_weighted_progress(showPrice)
     else:
-        progress = (float(showPrice) - float(min_price)) / (float(max_price) - float(min_price))
+        progress = (float(showPrice) - float(g_min_price)) / (float(g_max_price) - float(g_min_price))
 
     interval_time = time.perf_counter() - start_time
     min, sec = extract_minutes_seconds(interval_time)
-    remain_min, remain_sec = extract_minutes_seconds(interval_time / progress - interval_time)
+    # remain_min, remain_sec = extract_minutes_seconds(interval_time / progress - interval_time)
 
     if tag == 0 and item_count != 0:  # Default 输出
         print(f'{Fore.CYAN}[INFO] 当前为第 {pause_count} 次暂停, 已爬取 {item_count} 件商品, 爬取进度 {progress * 100:.2f} %{Style.RESET_ALL}')
-        print(f'{Fore.GREEN}[INFO] 程序已运行 {min} 分 {sec} 秒, 预计还将运行 {remain_min} 分 {remain_sec} 秒{Style.RESET_ALL}')
+        # print(f'{Fore.GREEN}[INFO] 程序已运行 {min} 分 {sec} 秒, 预计还将运行 {remain_min} 分 {remain_sec} 秒{Style.RESET_ALL}')
     elif tag == 1 and item_count != 0:  # Error 412 输出
         print(f'{Fore.CYAN}[INFO] 当前为第 {pause_count} 次暂停, 已爬取 {item_count} 件商品, 爬取进度 {progress * 100:.2f} %{Style.RESET_ALL}') if retry_count == 0 else print(f'{Fore.CYAN}[INFO] 当前为第 {retry_count} 次重试, 第 {pause_count} 次暂停, 已爬取 {item_count} 件商品, 爬取进度 {progress * 100:.2f} %{Style.RESET_ALL}')
-        print(f'{Fore.GREEN}[INFO] 程序已运行 {min} 分 {sec} 秒, 预计还将运行 {remain_min} 分 {remain_sec} 秒{Style.RESET_ALL}')
+        # print(f'{Fore.GREEN}[INFO] 程序已运行 {min} 分 {sec} 秒, 预计还将运行 {remain_min} 分 {remain_sec} 秒{Style.RESET_ALL}')
     elif tag == 2 and item_count != 0:  # KeyboardInterrupt 输出
         print(f'{Fore.CYAN}[INFO] 当前已爬取 {item_count} 件商品, 爬取进度 {progress * 100:.2f} %{Style.RESET_ALL}')
     elif tag == 3:  # DONE 输出
-        print(f'{Fore.GREEN}[DONE] 爬取进程结束, 本次在 {min_price}-{max_price} 元的区间内共爬取到 {item_count} 件商品, 耗时 {min} 分 {sec} 秒!{Style.RESET_ALL}')
+        print(f'{Fore.GREEN}[DONE] 爬取进程结束, 本次在 {g_min_price}-{g_max_price} 元的区间内共爬取到 {item_count} 件商品, 耗时 {min} 分 {sec} 秒!{Style.RESET_ALL}')
         exit() if item_count == 0 else ""
 
 def time_sleep_5s():
@@ -322,44 +404,57 @@ def except_error_null(e):
     print(f'{Fore.MAGENTA}{"-" * 70}{Style.RESET_ALL}')
     print(f'{Fore.RED}[WARN] 程序退出时存在未解决的异常, 爬取的商品信息可能不完整!{Style.RESET_ALL}')
 
-colorama.init(autoreset = True)
-window = tk.Tk()
-window.title("B站魔力赏市集商品信息爬取器")
-window.geometry("600x400")
 
-# 最低价格标签和输入框
-min_price_label = tk.Label(window, text = "最低价格(默认 0 元)")
-min_price_label.pack()
-min_price_entry = tk.Entry(window)
-min_price_entry.pack()
+# 跳出爬取数据条件
+def is_to_end_crawler()-> bool:
+    if bilimart.curr_match_time > bilimart.max_match_time or bilimart.curr_search_time > MAX_SEARCH_TIME :
+        return True
+    return False
 
-# 最高价格标签和输入框
-max_price_label = tk.Label(window, text = "最高价格(默认 5000 元)")
-max_price_label.pack()
-max_price_entry = tk.Entry(window)
-max_price_entry.pack()
+# 判断是否匹配关键字
+def is_match_keywords(item_name: str)-> bool:
+    for keyword in bilimart.keywords:
+        if re.search(keyword, item_name):
+            bilimart.curr_match_time += 1
+            return True
+    return False
 
-# 搜索类别标签和输入框
-category_label = tk.Label(window, text = "搜索类别(默认全选, 可选: 手办、模型、周边、3C、福袋)")
-category_label.pack()
-category_entry = tk.Entry(window)
-category_entry.pack()
 
-# 最低折扣标签和输入框
-min_discount_label = tk.Label(window, text = "最低折扣(默认 0, 范围: 0-100)")
-min_discount_label.pack()
-min_discount_entry = tk.Entry(window)
-min_discount_entry.pack()
+def set_bot_matcher(mc: Matcher):
+    global matcher
+    matcher = mc
 
-# 最高折扣标签和输入框
-max_discount_label = tk.Label(window, text = "最高折扣(默认 100, 范围: 0-100)")
-max_discount_label.pack()
-max_discount_entry = tk.Entry(window)
-max_discount_entry.pack()
+def set_min_price(min_price: float):
+    bilimart.min_price = min_price
 
-# 爬取按钮
-crawl_button = tk.Button(window, text = "开始爬取", command = crawler)
-crawl_button.pack()
+def set_max_price(max_price: float):
+    bilimart.max_price = max_price
 
-window.mainloop()
-colorama.deinit()
+def append_keyword(keyword: str):
+    if keyword != "":
+        bilimart.keywords.append(keyword)
+
+
+#返回获取到的商品信息字符串
+async def get_match_items_str()-> str:
+    global g_min_price, g_max_price, min_discount, max_discount, min_discount
+    colorama.init(autoreset=True)
+    if bilimart.min_price is not None:
+        g_min_price = bilimart.min_price
+    if bilimart.max_price is not None:
+        g_max_price = bilimart.max_price
+    if bilimart.min_discount is not None:
+        min_discount = bilimart.min_discount
+    if bilimart.max_discount is not None:
+        max_discount = bilimart.max_discount
+
+    if matcher is None:
+        bilimart.reset()
+        raise ValueError("matcher 不能为空")
+
+    # 爬取主函数
+    await crawler()
+
+    bilimart.reset()
+    colorama.deinit()
+    return bilimart.print()
