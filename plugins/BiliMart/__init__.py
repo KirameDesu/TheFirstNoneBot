3f61c2a29f6b6060689c2509ba81eca1d22f170e
RFN_KIRAME <13787701007@sina.cn>
2024-10-18 16:07:46 +0800

    1.基本实现本地数据库的读取与写入

--- plugins/BiliMart/__init__.py
+++ plugins/BiliMart/__init__.py
@@ -1,19 +1,16 @@
 import re
 import argparse
 import shlex
-from logging import lastResort
 
 from nonebot.adapters import Message
-from nonebot.adapters import Event
 from nonebot.internal.params import ArgPlainText, Arg
-from nonebot.log import default_format
 from nonebot.params import CommandArg, Received, LastReceived
 from nonebot.matcher import Matcher
 
 from nonebot import on_command, logger
-from starlette.config import undefined
 
-from plugins.BiliMart.bilimart import get_match_items_str, append_keyword, set_min_price, set_max_price, set_bot_matcher
+from plugins.BiliMart.bilimart import update_database, append_keyword, set_min_price, set_max_price, \
+    set_bot_matcher, bilimart, update, get
 
 pattern1 = r'--minprice\s+\d+(\.\d+)?\s*--maxprice\s+\d+(\.\d+)?\s*(--keyword\s+.+\s*)?$'
 pattern2 = r'123123'
@@ -22,78 +19,95 @@ pattern2 = r'123123'
 响应信息定义
 '''
 mc_biliMart = on_command("bilimart")
+mc_biliMart_update_db = on_command("bilimart update")
+mc_biliMart_require_db = on_command("bilimart get")
 
 
-@mc_biliMart.handle()
-async def _(marcher: Matcher, args: Message = CommandArg()):
-    set_bot_matcher(marcher)
-    if para := args.extract_plain_text():
-        if para == "go":
-            # await mc_biliMart.finish(f"关键字{para}");
-            await mc_biliMart.finish(get_match_items_str())
-        else:
-            try:
-                args = parse_command_line(para)
-                if args.keyword:
-                    await mc_biliMart.send("开始搜索~关键字为\"" + str(args.keyword) + "\"")
-                else:
-                    await mc_biliMart.send(f"准备更新数据库~金额范围为{args.minprice}到{args.maxprice}")
-                # BiliMart搜索主程序
-                matched_str = do_bilimart(args.minprice, args.maxprice, args.keyword)
-                if matched_str != "":
-                    await mc_biliMart.finish(matched_str)
-                else:
-                    await mc_biliMart.finish("什么都没有找到哦")
-            except ValueError:
-                await mc_biliMart.finish("这是不被识别的指令格式哦")
-            except AttributeError:
-                await mc_biliMart.finish("参数错了哦")
-
-
+#=============================================================
+#                  指令-----"/bilimart"
+#=============================================================
 @mc_biliMart.got("keyword", prompt="给我一个关键字")
 async def _(keyword: str = ArgPlainText()):
     logger.info("keyword=" + keyword)
     if not re.fullmatch(r'[\u4e00-\u9fff]+', keyword):
-        await mc_biliMart.reject("只能输入中文哦")
+        await mc_biliMart_update_db.reject("只能输入中文哦")
 
 
 @mc_biliMart.got("min_money", prompt="最小金额是多少呢")
 async def _(min_money: str = ArgPlainText()):
     logger.info("max_money=" + min_money)
     if not re.fullmatch(r'[0-9]+', min_money):
-        await mc_biliMart.reject("只能输入数字哦")
+        await mc_biliMart_update_db.reject("只能输入数字哦")
 
 
 @mc_biliMart.got("max_money", prompt="最大金额是多少呢")
 async def _(matcher: Matcher, max_money: str = ArgPlainText()):
     logger.info("max_money=" + max_money)
     if not re.fullmatch(r'[0-9]+', max_money):
-        await mc_biliMart.reject("只能输入数字哦")
+        await mc_biliMart_update_db.reject("只能输入数字哦")
     else:
         minp = matcher.get_arg("min_money", default=0)
         maxp = matcher.get_arg("max_money", default=1500)
         kw = matcher.get_arg("keyword", default="")
-        await mc_biliMart.finish(await do_bilimart(minp, maxp, kw))
+        await mc_biliMart_update_db.finish(update(minp, maxp, kw))
 
 
-def do_bilimart(min_price: float, max_price: float, keyword: str) -> str:
-    set_min_price(min_price)
-    set_max_price(max_price)
-    append_keyword(keyword)
-    logger.info(f"开始查询最小金额{min_price},最大金额{max_price},关键词\"{keyword}\"的商品...")
+#=============================================================
+#                  指令-----"/bilimart update"
+#=============================================================
+@mc_biliMart_update_db.handle()
+async def _(matcher: Matcher, args: Message = CommandArg()):
+    set_bot_matcher(matcher)
+    if para := args.extract_plain_text():
+        if para == "go":
+            await mc_biliMart_update_db.finish(update_database())
+        else:
+            try:
+                parse_args = parse_command_line(para)
+                if parse_args.keyword:
+                    # await mc_biliMart_update_db.send("开始搜索关键字为\"" + str(parse_args.keyword) + "\"的商品")
+                    await mc_biliMart_update_db.finish("这种指令暂时被屏蔽了哦")
+                else:
+                    await mc_biliMart_update_db.send(f"准备更新数据库~金额范围为{parse_args.minprice}到{parse_args.maxprice}")
+                try:
+                    # BiliMart搜索主程序
+                    matched_str = update(parse_args.minprice, parse_args.maxprice, parse_args.keyword)
+                    await mc_biliMart_update_db.finish(matched_str)
+                except ValueError as e:
+                    await mc_biliMart_update_db.finish("ERROR<" + str(e) + ">")
+            except ValueError:
+                await mc_biliMart_update_db.finish("这是不被识别的指令格式哦")
+            except AttributeError:
+                await mc_biliMart_update_db.finish("参数错了哦")
+
+
+#=============================================================
+#                  指令-----"/bilimart get"
+#=============================================================
+@mc_biliMart_require_db.handle()
+async def _(matcher: Matcher, args: Message = CommandArg()):
+    set_bot_matcher(matcher)
     try:
-        item_str = get_match_items_str()
-        return item_str
-    except ValueError as e:
-        logger.error(f"爬取函数执行出错--{e}")
-        return ""
+        parse_args = parse_command_line(args.extract_plain_text())
+        if parse_args.keyword:
+            await mc_biliMart_update_db.send("开始搜索关键字为\"" + str(parse_args.keyword) + "\"的商品")
+        else:
+            await mc_biliMart_update_db.finish(f"要给个关键字哦, 例如--keyword '关键字'")
+        try:
+            # BiliMart搜索主程序
+            matched_str = get(parse_args.minprice, parse_args.maxprice, parse_args.keyword)
+            await mc_biliMart_update_db.finish(matched_str)
+        except ValueError as e:
+            await mc_biliMart_update_db.finish("ERROR<" + str(e) + ">")
+    except ValueError:
+        await mc_biliMart_update_db.finish("这是不被识别的指令格式哦")
+    except AttributeError:
+        await mc_biliMart_update_db.finish("参数错了哦")
 
 
-# @mc_biliMart.receive()
-# async def receive_func(event: Event = LastReceived()):
-#     if event.get_type() == "message":
-#         await mc_biliMart.reject("戳我一下")
-#     await mc_biliMart.finish("Roger~")
+#=============================================================
+#                       Handle Function
+#=============================================================
 
 
 def normal_search(input_str):
@@ -108,7 +122,6 @@ def normal_search(input_str):
     # 解析参数
     args = parser.parse_args(args_list)
     return args
-
 
 
 def parse_command_line(input_str):
